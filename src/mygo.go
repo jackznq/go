ee9da545fd10714cb3d59bbd7d3de5c19d035625
jackznq <zhangnaiquan@qjdchina.com>
2019-08-23 12:51:34 +0800

    znq

---
+++ src/mygo.go
@@ -0,0 +1,18 @@
+package main
+import (
+  "fmt"
+)
+
+func main(){
+	var (
+		name string
+		age int
+		location string
+	)
+	name = "znq"
+	age =28
+	location = "hangzhou"
+	fmt.Printf(name,age,location):
+fmt.Printf("hello.world")
+
+}
---
+++ src/variables.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "fmt"
+)
+
+func main() {
+	var (
+	name     = "Prince Oberyn"
+	
+)
+_ =name
+ // fmt.Println(name,age,location)
+ // fmt.Println()
+}
